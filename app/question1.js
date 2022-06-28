7eb4f50528dca4034c9d75104e429bb67c8615a2
AzureKn1ght <philsonnah@msn.com>
2022-06-29 01:35:34 +0800

    needs renaming

--- app/question1.js
+++ app/question1.js
@@ -1,10 +1,10 @@
 /*
-- Problem 1 - 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+- Problem 7 - 
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
+What is the 10 001st prime number?
 
-Source: https://projecteuler.net/problem=1
+Source: https://projecteuler.net/problem=7
 */
 
 // Function to find the nth prime number
